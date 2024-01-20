b8df8ad69c3edba62c4e965c561289b6693c7c87
pkurto16 <ptkurto3@gmail.com>
2024-01-19 23:35:08 -0500

    QOL Chat box

--- Boiler-Schedulings-Frontend/src/components/Chat/ChatWindow/ChatWindow.jsx
+++ Boiler-Schedulings-Frontend/src/components/Chat/ChatWindow/ChatWindow.jsx
@@ -1,22 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './ChatWindow.css';
 
 function ChatWindow() {
     const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+    const messagesEndRef = useRef(null);
 
-    const sendMessage = (message) => {
-        setMessages([...messages, message]);
-        document.getElementById("ChatBox").value = "";
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+
+    useEffect(scrollToBottom, [messages]);
+
+    const handleSendMessage = (e) => {
+        if (e.key === 'Enter') {
+            document.getElementById("ChatBox").value = "";
+            const newEntry = {
+                text: newMessage,
+                timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' })
+            };
+            setMessages([...messages, newEntry]);
+            setNewMessage('');
+
+        }
     };
 
     return (
         <div className="chat-window-container">
             <div className="messages-container">
                 {messages.map((message, index) => (
-                    <div key={index} className="message">{message}</div>
+                    <div key={index} className="message">
+                        <div>{message.text}</div>
+                        <div className="timestamp">{message.timestamp}</div>
+                    </div>
                 ))}
+                <div ref={messagesEndRef} />
             </div>
-            <input id = "ChatBox" placeholder="Enter a message..." type="text" onKeyDown={(e) => e.key === 'Enter' && sendMessage(e.target.value)} />
+            <input
+                id = "ChatBox"
+                type="text"
+                placeholder="Enter a message..."
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                onKeyDown={handleSendMessage}
+            />
         </div>
     );
 }
