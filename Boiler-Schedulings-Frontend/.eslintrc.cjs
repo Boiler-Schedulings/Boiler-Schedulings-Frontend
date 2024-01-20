85cda215f43caeb99b6f8f6680d5648682a0555d
pkurto16 <ptkurto3@gmail.com>
2024-01-20 13:49:34 -0500

    env

--- Boiler-Schedulings-Frontend/.eslintrc.cjs
+++ Boiler-Schedulings-Frontend/.eslintrc.cjs
@@ -1,6 +1,14 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true,
+    API_KEY: undefined,
+    AUTH_DOMAIN: undefined,
+    PROJECT_ID: undefined,
+    STORAGE_BUCKET: undefined,
+    MESSAGING_SENDER_ID: undefined,
+    APP_ID: undefined,
+    MEASUREMENT_ID: undefined,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
--- Boiler-Schedulings-Frontend/src/main.jsx
+++ Boiler-Schedulings-Frontend/src/main.jsx
@@ -3,7 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const apiKey = process.env.FIREBASE_API_KEY;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import process from "../.eslintrc.cjs";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
